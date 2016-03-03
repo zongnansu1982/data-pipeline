56ab0cde2e55a880a4a4b55315cc52e9fed9683d
Ted Ochiai <tedochiai@yahoo.com>
2016-03-03 13:04:59 -0800

    Updated for general use

---
+++ ingestors/NDAR/Data From Papers/GetAllIndividualPaperHTML.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import requests, bs4, time, os
+
+
+def clean(txt):
+    txt = txt.replace('"', "'")
+    escape = txt.find("\\")
+    while not escape == -1:
+        txt = txt[:escape] + txt[escape+2:]
+        escape = txt.find("\\")
+
+    return txt
+
+def run():
+    print "Getting all HTML files ..."
+    page = open("DataFromPapers.html", "r").read()
+
+    soup = bs4.BeautifulSoup(page, "html.parser")
+
+    # List of homepages
+    homepages = []
+
+    # Every home page starts with this text followed by the page's id number
+    noIdPage = "https://ndar.nih.gov/study.html?id="
+
+    # Puts the html for each entry number into a list
+    pageIDs = soup.find_all('span', {"class" : "model-id"})
+    totalEntries = len(pageIDs)
+
+    for index in range(len(pageIDs)):
+        entry = requests.get(noIdPage + pageIDs[index].text)
+        directory = "Data From Papers HTML"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        f = open(directory + "/Entry" + pageIDs[index].text + ".html", "w")
+
+        for line in entry:
+            f.write(line)
+
+        f.close()
+        totalEntries -= 1
+        print "Finished writing Entry" + pageIDs[index].text + ".HTML, " , \
+            totalEntries, " entries left"
+
+    print "Done."
---
+++ ingestors/NDAR/Data From Papers/Run All.py
@@ -0,0 +1,6 @@
+# Handles all processes necessary to acquire all JSON files for Data from Papers
+import GetDataFromPapersHTML, GetAllIndividualPaperHTML, GetAllIndividualPaperJSON
+
+GetDataFromPapersHTML.run()
+GetAllIndividualPaperHTML.run()
+GetAllIndividualPaperJSON.run()
